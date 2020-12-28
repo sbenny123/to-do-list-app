4769d1caa871b91ecedf99473744c4da76cebafd
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2020-12-28 11:59:52 +0000

    Perform Hello World test

--- app.js
+++ app.js
@@ -2,4 +2,9 @@
 const express = require("express");
 const app = express();
 
-app.listen(process.env.port || 3000, () => console.log("Server running"));
+
+app.get('/', (req,res) => {
+    res.send("Hello World");  
+});
+
+app.listen(process.env.port || 3000, () => console.log("Server running on port " + (process.env.port || 3000)));
