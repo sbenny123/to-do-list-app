d225a8c282e81ebd3ca508cd2ff145ada1606098
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2020-12-31 13:08:13 +0000

    Remove old test file

--- Tests/test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("../app");
-
-
-describe("GET /", () => {
-    it("respond with Hello World", (done) => {
-      request(app).get("/").expect("Hello World", done);
-    })
-});
