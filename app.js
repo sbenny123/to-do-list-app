870aa6af12f6e1ebc0459001dc77882746e59d7b
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2020-12-31 12:24:42 +0000

    Create bin/www file
    For starting application

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+/**
+ * The main application file:
+ *  - Handles application errors
+ *  - Used in showing error pages and logging
+ */
 
-const express = require("express");
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = (process.env.PORT || 3000);
+let app = express();
 
 
-app.get('/', (req, res) => {
-    res.send("Hello World");  
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Used to handle reading data from <form> element
+app.use(bodyParser.urlencoded({ extended : true }));
+
+
+app.get('/', (request, response) => {
+    // Show temporary index file for now
+    response.sendFile(__dirname + '/index.html');  
 });
 
-app.listen(PORT, () => console.log("Server is running on port " + PORT));
+app.listen(PORT, function() {   
+    console.log("Server is running on port " + PORT)
+});
 
 module.exports = app;
