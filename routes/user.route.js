2cccd07756ebcfb9ca221e5d65b5bc51dbe1c7f7
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2021-01-11 12:18:41 +0000

    Update list-view template
    Update input box and view of lists

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,11 +4,11 @@
 
 // Module dependencies
 const express = require('express');
+const passport = require('passport');
 
 const router = express.Router(); // Express Router object
 const userController = require('../controllers/user.controller'); // User controller
 
-const passport = require('passport');
 
 // Get registration page and register user
 router.get('/register', userController.showRegistration);
