e1ccdbe82b6a228a2e0c5cd714fe3b8acd56f1e3
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2021-01-12 22:45:39 +0000

    Update styling for lists and tasks

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -43,12 +43,12 @@ $(function(){
 
            for (var i = 0; i < data.length; i++) {
                content +=
-                   `<li class="list-group-item">
+                   `<li class="list-group-item list-group-item-action">
                         <div class="row">
                             <div class="col-10">                           
-                                <a href="/tasks/${data[i].list_id}">${data[i].name}</a>
+                                <a class="text-decoration-none" href="/tasks/${data[i].list_id}">${data[i].name}</a>
                             </div>
-                            <div class="col-2">
+                            <div class="col-2 btn-right">
                                 <button type="button" class="btn btn-primary" id="btn_delete" onclick="deleteList('${data[i].list_id}', '${data[i].user_id}')">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -63,12 +63,12 @@ $(function(){
 
         } else {    
             content +=
-                `<div class="text-center">
+                `<div class="empty-state text-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
                         <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
                         <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                     </svg>
-                    <div class="empty-state">
+                    <div>
                         <p class="empty-state-heading">Looks like you don't have any lists</p>
                         <p class="empty-state-description">Add a list using the input box above</p>
                     </div>
--- public/scripts/task.js
+++ public/scripts/task.js
@@ -56,7 +56,7 @@ $(function(){
                 </span>
             </label>
         </div>
-        <div class="dropdown col-6 col-md-4">
+        <div class="col-6 col-md-4 btn-right">
             <button type="button" class="btn btn-primary" id="btn_delete" onclick="deleteTask('${data[i]._id}', '${data[i].list_id}')">
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -70,12 +70,12 @@ $(function(){
             
         } else {
             content +=
-            `<div class="text-center">
+            `<div class="empty-state text-center">
                 <svg xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
                     <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
                     <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
                 </svg>
-                <div class="empty-state">
+                <div>
                     <p class="empty-state-heading">Looks like you don't have any tasks</p>
                     <p class="empty-state-description">Add a task using the input box above</p>
                 </div>
